f726f4533c450c71b7254827c64c3161fbde9019
DeniseTenchiu <158043271+DeniseTenchiu@users.noreply.github.com>
2024-02-01 13:32:59 +0000

    hey

--- ex19.c
+++ ex19.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
-#include <string.h>
 int main ()
 {
     char parola [100];
-    char x;
+    int x=0;
     printf ("Scrivi una parola in minuscolo\n");
-    scanf("%*s", parola);
-    while ( parola [x] !=0)
+    scanf(" %[^\n]", parola);
+    while ( parola [x] != '\0')
     {
         if (parola [x]>= 97 && parola[x]<=122)
         {
             parola[x]-=32;
-            printf ("%c", parola [x]);
+            
         }
+        printf ("%c", parola [x]);
         x=x+1;
     }
     return (0);
